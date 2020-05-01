Various small bug fixes, esp for uncertainty computation

lib/datasets/waymo_lidb.py
@@ -12,7 +12,7 @@ import os
 import json
 from datasets.db import db
 # import datasets.ds_utils as ds_utils
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
 import numpy as np
 import scipy.sparse
 # import scipy.io as sio

lib/layer_utils/snippets.py
@@ -22,7 +22,7 @@ def generate_anchors_pre(height,
     anchor_scales = np.array(anchor_scales)*frame_scale
     anchor_ratios = np.array(anchor_ratios)
     anchors = generate_anchors(
-        base_size=feat_stride,ratios=anchor_ratios, scales=anchor_scales)
+        ratios=anchor_ratios, scales=anchor_scales)
     A = anchors.shape[0]
     shift_x = np.arange(0, width) * feat_stride
     shift_y = np.arange(0, height) * feat_stride

lib/model/config.py
@@ -18,7 +18,7 @@ cfg = __C
 #Need to turn this on in order to debug
 __C.DEBUG                    = edict()
 
-__C.DEBUG.EN                 = True
+__C.DEBUG.EN                 = False
 
 __C.DEBUG.DRAW_ANCHORS       = False
 __C.DEBUG.DRAW_ANCHOR_T      = False
@@ -202,7 +202,7 @@ __C.TRAIN.LIDAR = edict()
 
 __C.TRAIN.IMAGE = edict()
 __C.TRAIN.LIDAR.BBOX_NORMALIZE_MEANS = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-__C.TRAIN.LIDAR.BBOX_NORMALIZE_STDS = (0.1, 0.1, 1.0, 0.2, 0.2, 1.0, 1.0)
+__C.TRAIN.LIDAR.BBOX_NORMALIZE_STDS = (0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.5)
 
 __C.TRAIN.IMAGE.BBOX_NORMALIZE_MEANS = (0.0, 0.0, 0.0, 0.0)
 __C.TRAIN.IMAGE.BBOX_NORMALIZE_STDS = (0.1, 0.1, 0.2, 0.2)
@@ -267,7 +267,7 @@ __C.RESNET.MAX_POOL = False
 
 # Number of fixed blocks during training, by default the first of all 4 blocks is fixed
 # Range: -1 (none) to 3 (all)
-__C.RESNET.FIXED_BLOCKS = -1
+__C.RESNET.FIXED_BLOCKS = 2
 
 #
 # MobileNet options
@@ -388,7 +388,7 @@ __C.LIDAR.ANCHOR_STRIDE = np.array([2,2,0.5])
 __C.LIDAR.NUM_BBOX_ELEM = 7
 
 __C.IMAGE = edict()
-__C.IMAGE.USE_FPN = False
+__C.IMAGE.USE_FPN = True
 __C.IMAGE.NUM_BBOX_ELEM = 4
 
 

lib/nets/imagenet.py
@@ -122,12 +122,6 @@ class imagenet(Network):
         if mode:
             # Set fixed blocks to be in eval mode (not really doing anything)
             self.resnet.eval()
-            #Disable as resnet has been pretrained
-            if(cfg.RESNET.FIXED_BLOCKS != -1):
-                self.resnet.apply(set_bn_eval)
-            else:
-                self.resnet.train()
-                self.resnet.apply(set_bn_train)
 
             if cfg.RESNET.FIXED_BLOCKS <= 3:
                 self.resnet.layer4.train()
@@ -138,6 +132,13 @@ class imagenet(Network):
             if cfg.RESNET.FIXED_BLOCKS <= 0:
                 self.resnet.layer1.train()
                 self.resnet.conv1.train()
+
+            #This goes after the case/switch above due to imagenet either having all batchnorm train(0-4) or all freeze (-1)
+            if(cfg.RESNET.FIXED_BLOCKS != -1):
+                self.resnet.apply(set_bn_eval)
+            else:
+                self.resnet.train()
+                self.resnet.apply(set_bn_train)
 
     def eval(self):
         nn.Module.eval(self)